e98f9cee8451d2fdefcc034abda8fe784b354b6c
eugene <eugene.cherepanov.84>
2022-09-17 23:56:28 +0500

    8 home task done

---
+++ Home_task8/Program.cs
@@ -0,0 +1,17 @@
+﻿bool Even_number (int num)
+{
+    bool result=true;
+    int number = num % 2;
+    if (number > 0) result=false ; 
+    return result;
+        
+}
+Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = 1;
+while (num1 <= num)
+{
+    bool num2 = Even_number(num1);
+    if (num2) Console.Write($"{num1}  ");
+    num1++;
+} 
