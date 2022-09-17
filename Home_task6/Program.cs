9d7ccbac900bf7ebdf40b7091bb5fce0e9c8ee4b
eugene <eugene.cherepanov.84>
2022-09-17 23:22:39 +0500

    6 home task done

---
+++ Home_task6/Program.cs
@@ -0,0 +1,20 @@
+﻿bool Even_number (int num)
+{
+    bool number=true;
+    int result = num % 2;
+    if (result > 0) number=false ; 
+    return number;
+        
+}
+
+Console.Write("Enter a number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+bool number2 = Even_number(number1);
+if (number2)
+{
+    Console.WriteLine($"Число четное:  {number2}");
+}
+else
+{
+    Console.WriteLine($"Число нечетное:  {number2}");
+}
