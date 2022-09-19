ebc9f0f102f8995ac47987e608ccfe821e914cb7
eugene <eugene.cherepanov.84>
2022-09-19 09:10:00 +0500

    4 task done

---
+++ Home_task4/Program.cs
@@ -0,0 +1,15 @@
+﻿int Max_number(int first_num, int second_num, int third_num)
+{
+    int result = first_num;
+    if (second_num > result) result = second_num;
+    if (third_num > result) result = third_num;
+    return result;
+}
+Console.WriteLine("Enter 1 number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter 2 number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter 3 number");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = Max_number(num1,num2,num3);
+Console.WriteLine($"Maximum number:  {max}");
