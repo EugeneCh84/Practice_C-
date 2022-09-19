15dd0698f65b473fbe93f3775d327440bdcba180
eugene <eugene.cherepanov.84>
2022-09-19 09:04:09 +0500

    2 task done

---
+++ Home_task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter 1 number:  ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 2 number:  ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+while (num_1 == num_2)
+{
+    Console.WriteLine("Numbers are the same, enter another number!");
+    num_2 = Convert.ToInt32(Console.ReadLine());
+}
+if ( num_1 > num_2)
+{
+    Console.WriteLine($"Number {num_1} > then number {num_2}");
+}
+else
+{
+    Console.WriteLine($"Number {num_2} > then number {num_1}");
+}
