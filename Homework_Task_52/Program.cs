b370b19a83baa25c53fb742a2deac0f67133a5d9
eugene <eugene.cherepanov.84>
2022-10-18 14:14:01 +0500

    done

---
+++ Homework_Task_52/Program.cs
@@ -0,0 +1,55 @@
+﻿//Task  # 52
+
+void ColumnsAverage()
+{
+    Console.WriteLine("Enter array size, M and N");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    int columns = Convert.ToInt32(Console.ReadLine());
+    int[,] numbers = new int[rows,columns];
+    FillArray(numbers,rows,columns);
+    PrintArray(numbers,rows,columns);
+    int[] result = new int[columns];
+    
+    for (int i=0; i<columns; i++)
+    {
+        
+        for (int j=0; j<rows; j++)
+        {
+            result[i] += numbers[j,i];
+        }
+        
+    }
+    Console.WriteLine("Average of columns");
+    foreach (double val in result)
+    {
+        Console.WriteLine(Math.Round(val/rows,2));
+        Console.WriteLine();
+    }
+}
+ColumnsAverage();
+
+
+void FillArray(int[,] numbers, int rows, int columns)
+{
+    Random random = new Random();
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(0, 10);
+        }
+    }
+}
+
+
+void PrintArray(int[,] numbers, int rows, int columns)
+{
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
